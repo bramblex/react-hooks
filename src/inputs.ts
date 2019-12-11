daedeb7a90697304f0f9f4b22f905ebe3c815389
乔健 <qjnight@gmail.com>
2019-12-11 14:54:10 +0800

    Update inputs.ts

--- src/inputs.ts
+++ src/inputs.ts
@@ -3,7 +3,7 @@ export type HooksInputs = any[] | undefined
 
 export function inputsChange(oldInputs: HooksInputs, newInputs: HooksInputs) {
     if (oldInputs && newInputs) {
-        if (oldInputs.length > 0 && oldInputs.length > 0) {
+        if (oldInputs.length > 0 && newInputs.length > 0) {
             if (oldInputs.length === newInputs.length) {
                 for (let i = 0, l = oldInputs.length; i < l; i++) {
                     if (oldInputs[i] !== newInputs[i]) {
